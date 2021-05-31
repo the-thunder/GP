bc5a30782e3553ac7f539ddbdf4f46ec6790d38a
Youssef Amir <48365320+the-thunder@users.noreply.github.com>
2021-05-31 15:57:15 +0200

    Upload

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:login_application_1/login_screen_3.dart';
+import 'package:login_application_1/misc/slide_list_view.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Login Screen 1',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomPadding: false,
+      backgroundColor: Colors.white,
+      body: Container(
+        child: LoginScreen3(),
+      ),
+    );
+  }
+
+  Container buildPage(Color color, String text) {
+    return Container(
+      color: color,
+      child: Center(
+        child: Text(
+          text,
+          style: TextStyle(
+            fontSize: 50.0,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
